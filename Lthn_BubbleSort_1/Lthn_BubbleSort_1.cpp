44a6291493f80e8cfb7abb14c89c93513f7c75b5
Dwanrh <dwanrh@gmail.com>
2023-03-18 23:55:39 +0700

    Membuat procedure untuk input

--- Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
+++ Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int a[20];                              // Deklarasi array a dengan ukuran 20
 int n;                                  // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+void input() {							// Procedure untuk input
 
+}
