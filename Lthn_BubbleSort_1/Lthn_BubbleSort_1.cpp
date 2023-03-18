2c6f3b09908bd1cb17d70fba9d6f86729242aa74
Dwanrh <dwanrh@gmail.com>
2023-03-19 00:51:38 +0700

    Looping dengan j dimulai dari 0 hingga n-1

--- Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
+++ Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
@@ -53,4 +53,7 @@ void display() {							// Procedure untuk menampilkan hasil
 	cout << "==================================" << endl;				// Output ke layar
 	cout << "Element Array yang telah  tersusun" << endl;				// Output ke layar
 	cout << "==================================" << endl;				// Output ke layar
+	for (int j = 0; j < n; j++) {			// Looping dengan j dimulai dari 0 hingga n-1
+
+	}
 }
