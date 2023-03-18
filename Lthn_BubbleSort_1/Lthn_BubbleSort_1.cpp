7e97bd6563ff3ab23d278ebf8769a4d9aeec76ce
Dwanrh <dwanrh@gmail.com>
2023-03-19 00:02:11 +0700

    Keluar dari loop

--- Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
+++ Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
@@ -10,5 +10,6 @@ void input() {							// Procedure untuk input
 		cout << "Masukan banyaknya elemen pada array : ";								// Output ke layar
 		cin >> n;						// Input dari pengguna
 		if (n <= 20)					// Jika n kurang dari atau sama dengan 20
+			break;						// Keluar dari loop
 	})
 }
