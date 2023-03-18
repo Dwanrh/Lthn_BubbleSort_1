f7e69c7ef3efdcaf0bc9fd7f1f895cd1f7dd4f3b
Dwanrh <dwanrh@gmail.com>
2023-03-18 23:57:41 +0700

    Membuat looping

--- Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
+++ Lthn_BubbleSort_1/Lthn_BubbleSort_1.cpp
@@ -6,5 +6,7 @@ int a[20];                              // Deklarasi array a dengan ukuran 20
 int n;                                  // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 void input() {							// Procedure untuk input
+	while (true {						// Looping
 
+	})
 }
